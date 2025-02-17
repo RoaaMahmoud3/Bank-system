887364e3e1c282edb78fdfcc6226b8807930026f
RoaaMahmoud3 <mroaa5188@gmail.com>
2025-02-17 22:54:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<string>
+#include"Validation.h"
 #include "Person.h"
 #include "Client.h"
 #include "Employee.h"
+#include "Admin.h"
 #include<fstream>
 using namespace std;
 
@@ -13,19 +15,19 @@ int main()
     file.close();*/
     /*ofstream file("Employee.txt");
     file.close();*/
-    ofstream file("Admin.txt");
-    file.close();
+    /*ofstream file("Admin.txt");
+    file.close();*/
     /*ofstream file("text.txt");
     file<<"this is my first\n";
     file<<"this is my first\n";
     file<<"this is my first\n";
     file.close();*/
 
-    /*Admin m;
+    Admin m;
     m.Setname(Validation::Entername());
     m.Setpassword(Validation::Enterpassword());
     m.Setid(Validation::Enterid());
     m.Setsalary(Validation::Entersalary());
     m.Display();
-    return 0;*/
+    return 0;
 }
